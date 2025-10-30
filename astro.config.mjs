00e8b152bb12b89239d5dc0eef610a9d2f54f391
unknown <samuelgalindop@gmail.com>
2025-10-30 22:20:52 +0100

    V0.25.30102321

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
-import vercel from '@astrojs/vercel';
+import vercel from '@astrojs/vercel/serverless';
+
 
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
   adapter: vercel({
-    mode: 'serverless',
   }),
   // Tailwind CSS se procesa a través de PostCSS (ver postcss.config.cjs),
   // no es necesario añadir plugins adicionales aquí.
